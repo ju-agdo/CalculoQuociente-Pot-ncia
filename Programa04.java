ee4027111633284cee5b633fee74c0a8a9ff5811
ju-agdo <juliana.agdo@gmail.com>
2022-11-28 20:43:15 -0300

    estudando3

--- Programa04.java
+++ Programa04.java
@@ -1,5 +1,32 @@
-public class Programa04 {
-    
+// Salvar como Programa04.java
+import javax.swing.*;
+
+class Programa04
+{
+public static void main (String entrada[])
+{
+
+// Declaração de variáveis
+int n1,n2,div;
+double pot;
+String msg="";
+
+// Estrada de Dados
+n1=
+Integer.parseInt(JOptionPane.showInputDialog ("Digite um número inteiro"));
+n2=
+Integer.parseInt(JOptionPane.showInputDialog("Digite outro número inteiro"));
+
+//processamento
+div = (int)n1/(int)n2;
+pot = Math.pow(n1,n2);
+//Saída de resultados
+msg= msg+ "O quociente da divisão de " + n1 + " por " + n2 + " = " + div + " \n ";
+msg = msg + "Potência de " + n1 + " por " + n2 + " = " + pot + " \n ";
+JOptionPane.showMessageDialog(null,msg);
+
+System.exit(0);
+
+}
 }
 
-magno      
